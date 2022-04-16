c35bafbbd2a72550bdd35b2a8cbe94b3a0e80832
CodingWon <kingcoding2298@gmail.com>
2022-04-16 22:50:34 +0900

    BoaderMapper : select 추가

--- src/test/java/org/zerock/sp1/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/sp1/mapper/BoardMapperTests.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.zerock.sp1.domain.Board;
 
+import java.util.List;
+
 @Log4j2
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations="file:src/main/webapp/WEB-INF/root-context.xml")
@@ -22,6 +24,12 @@ public class BoardMapperTests {
         Board board = Board.builder().title("제목").content("내용").writer("user1").build();
         log.info(board);
         boardMapper.insert(board);
+    }
+
+    @Test
+    public void testSelctList(){
+        List<Board> boardList = boardMapper.selectList();
+        boardList.forEach(board -> log.info(board));
     }
 
 }
